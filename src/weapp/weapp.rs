7811f7f1a058967966eaf9060673521177f50be4
shaipe <shaipe@sina.com>
2020-06-11 19:48:37 +0800

    aldfj

--- src/weapp/weapp.rs
+++ src/weapp/weapp.rs
@@ -1,17 +1,15 @@
 //! copyright
 //! 微信小程序接口对接
-//! 
-use byteorder::{NativeEndian, ReadBytesExt};
-use std::io::Cursor;
-use crate::{WeChatResult,Client};
-use crate::{aes256_cbc_decrypt,aes128_cbc_decrypt};
+//!
+//!
+use crate::aes128_cbc_decrypt;
+use crate::{Client, WeChatResult};
 const API_DOMAIN: &'static str = "https://api.weixin.qq.com";
-pub struct WeApp{}
+pub struct WeApp {}
 
-impl WeApp
-{
+impl WeApp {
     pub fn new() -> Self {
-        WeApp{}
+        WeApp {}
     }
 
     /// 注册一个小程序
@@ -20,7 +18,13 @@ impl WeApp
         Ok("".to_string())
     }
     ///获取session_key
-    pub async fn get_session_key(&self,appid:&str,code:&str,component_appid:&str,component_access_token:&str)-> WeChatResult<serde_json::Value>{
+    pub async fn get_session_key(
+        &self,
+        appid: &str,
+        code: &str,
+        component_appid: &str,
+        component_access_token: &str,
+    ) -> WeChatResult<serde_json::Value> {
         let url = format!("{api}/sns/component/jscode2session?appid={appid}&js_code={code}&grant_type=authorization_code&component_appid={component_appid}&component_access_token={component_access_token}",
         api=API_DOMAIN,
         appid=appid,
@@ -29,7 +33,7 @@ impl WeApp
         component_access_token=component_access_token
     );
         let api = Client::new();
-        let res= api.get(&url).await?;
+        let res = api.get(&url).await?;
         match api.json_decode(&res) {
             Ok(_data) => Ok(_data),
             Err(err) => {
@@ -38,16 +42,21 @@ impl WeApp
         }
     }
     ///获取手机号
-    pub  fn get_phone_num(&self,session_key:&str,iv:&str,encrypte_data:&str)->WeChatResult<String>{
+    pub fn get_phone_num(
+        &self,
+        session_key: &str,
+        iv: &str,
+        encrypte_data: &str,
+    ) -> WeChatResult<String> {
         let encrypte_data = base64::decode(encrypte_data).unwrap();
         let iv = base64::decode(iv).unwrap();
         let session_key = base64::decode(session_key).unwrap();
-        println!("session_key={:?}",session_key);
+        println!("session_key={:?}", session_key);
         // aes descrypt
         let res = aes128_cbc_decrypt(&encrypte_data, &session_key, &iv);
-        println!("res={:?}",res);
-        let text=match res{
-            Ok(v)=>{
+        println!("res={:?}", res);
+        let text = match res {
+            Ok(v) => {
                 // let mut rdr = Cursor::new(v[16..20].to_vec());
                 // let content_length = u32::from_be(rdr.read_u32::<NativeEndian>().unwrap()) as usize;
                 // println!("content_length={}",content_length);
@@ -55,27 +64,22 @@ impl WeApp
 
                 // let content_string = String::from_utf8(content.to_vec()).unwrap();
                 //content_string
-                match String::from_utf8(v){
-                    Ok(s)=>s,
-                    Err(_)=>"".to_owned()
+                match String::from_utf8(v) {
+                    Ok(s) => s,
+                    Err(_) => "".to_owned(),
                 }
-                
-            },
-            Err(_)=>{
-                "".to_owned()
             }
+            Err(_) => "".to_owned(),
         };
-        println!("{:?}",text);
+        println!("{:?}", text);
 
-        
         Ok(text)
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::{WeApp,aes128_cbc_decrypt,aes256_cbc_decrypt};
-    
+    use super::aes128_cbc_decrypt;
     #[test]
     // async fn test_get_session_key() {
     //     let bll=WeApp::new();
@@ -84,39 +88,31 @@ mod tests {
     //     "wx8c02ccf68ba12555",
     //     "33_M7sY_S52QXyTOOitEkOs-xIYEHVXyn9ZsMTc-Hp9wl4paiLn4kibeR9Ym17zP426FKlgxLVaIlp1IEdBbKs7X6M2lJdURJ_Wb85BrVa3Bf5ypFdC2rN-jzQHfzDfrLr2mXlR_ecq1IH3IW5WZKYhAAANYM"
     // ).await;
-       
 
     //     println!("decrypted={:?}", res);
     // }
-
     #[test]
-     fn test_get_phone_num(){
+    fn test_get_phone_num() {
         let encrypte_data="6iR9pNmvMgm6L+9NIaumsyqtEzvMgeyCZ+AQLTqjwxwirrgxZJXKvRyDCD3cU97hcVN1bzZrTYryxul40TrBqneGDjyiV4wVO+mF+Vx9xC9Sxa9FmMB+Eqgt47Wy5Qr7Jy/YvTFffxxsGpTT01zreQUov7N3tGFckWWAr+FqO3UbtRuGrTdEjX7TrtTl6jTUoJ57e9rsLL6SEfdQ6omliw==";
-        let session_key="QCTvQOXnZugJc4XezoCKUQ==";
-        let iv="M/Omw6FuQ0YU8Z2KAibRLA==";
+        let session_key = "QCTvQOXnZugJc4XezoCKUQ==";
+        let iv = "M/Omw6FuQ0YU8Z2KAibRLA==";
         let encrypte_data = base64::decode(encrypte_data).unwrap();
         let iv = base64::decode(iv).unwrap();
         let session_key = base64::decode(session_key).unwrap();
-        println!("session_key={:?}",session_key);
+        println!("session_key={:?}", session_key);
         // aes descrypt
         let res = aes128_cbc_decrypt(&encrypte_data, &session_key, &iv[..16]);
-        println!("res={:?}",res);
-        let text=match res{
-            Ok(v)=>{
-               
-                match String::from_utf8(v){
-                    Ok(s)=>{
-                        println!("{:?}",s);
-                        s
-                    },
-                    Err(_)=>"".to_owned()
+        println!("res={:?}", res);
+        let text = match res {
+            Ok(v) => match String::from_utf8(v) {
+                Ok(s) => {
+                    println!("{:?}", s);
+                    s
                 }
-                
+                Err(_) => "".to_owned(),
             },
-            Err(_)=>{
-                "".to_owned()
-            }
+            Err(_) => "".to_owned(),
         };
-        println!("{:?}",text);
+        println!("{:?}", text);
     }
 }
