94fcc3a0857fce2eb925543fe712e5b641bc8471
shaipe <shaipe@live.com>
2021-03-12 14:25:58 +0800

    update package and add req proxy code

--- web/src/wx_msg.rs
+++ web/src/wx_msg.rs
@@ -40,6 +40,58 @@ pub async fn message_reply(msg: &Message) -> Result<HttpResponse> {
         .body("success"))
 }
 
+
+// async fn forward(
+//     req: HttpRequest,
+//     pr: web::Data<Proxy>,
+//     body: web::Bytes,
+//     // url: web::Data<Url>,
+//     client: web::Data<Client>,
+// ) -> Result<HttpResponse, Error> {
+//     // println!("{:?}", pr);
+
+//     // 代理请求目标主机集群
+//     let targets = &pr.target;
+//     // 获取第一个目标主机
+//     let target_url = &targets[0];
+//     // 创建一个可变的url地址
+//     let mut new_url = Url::parse(&target_url.url).unwrap();
+
+//     let url_path = req.uri().path();
+//     let start_index = pr.path.len();
+//     // 设置请求的路径,并去掉代理目录前缀
+//     new_url.set_path(&url_path[start_index..]);
+//     new_url.set_query(req.uri().query());
+//     // let mut new_url = url.get_ref().clone();
+//     // new_url.set_path(req.uri().path());
+//     // new_url.set_query(req.uri().query());
+
+//     println!("{:?}", new_url);
+
+//     // TODO: This forwarded implementation is incomplete as it only handles the inofficial
+//     // X-Forwarded-For header but not the official Forwarded one.
+//     let forwarded_req = client
+//         .request_from(new_url.as_str(), req.head())
+//         .no_decompress();
+//     let forwarded_req = if let Some(addr) = req.head().peer_addr {
+//         forwarded_req.header("x-forwarded-for", format!("{}", addr.ip()))
+//     } else {
+//         forwarded_req
+//     };
+
+//     let mut res = forwarded_req.send_body(body).await.map_err(Error::from)?;
+
+//     let mut client_resp = HttpResponse::build(res.status());
+//     // Remove `Connection` as per
+//     // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Connection#Directives
+//     for (header_name, header_value) in res.headers().iter().filter(|(h, _)| *h != "connection") {
+//         client_resp.header(header_name.clone(), header_value.clone());
+//     }
+
+//     Ok(client_resp.body(res.body().await?))
+// }
+
+
 /// 代理消息业务转发
 pub async fn proxy_reply(app_id: &str, req: HttpRequest, body: web::Bytes) -> Result<HttpResponse> {
     use crate::cluster::get_domain;
