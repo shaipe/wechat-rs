071759ce3319fd7570005d8be03e58aea6a32781
xy <xy@n.local>
2022-04-02 13:58:13 +0800

    增加apP二次签名

--- pay/src/order.rs
+++ pay/src/order.rs
@@ -2,6 +2,7 @@
 use wechat_sdk::{Client, WechatResult};
 use itertools::Itertools;
 use wechat_sdk::Config;
+use chrono::prelude::{ Utc };
 pub struct Order{
 
 }
@@ -28,12 +29,13 @@ impl Order {
     pub async fn create(mut params: serde_json::Value) -> WechatResult<serde_json::Value> {
         let api_url = "https://api.mch.weixin.qq.com/pay/unifiedorder";
         let conf = Config::get();
-        //println!("ss:{:?}",conf);
+       
+        
         params["appid"] = serde_json::Value::String(conf.app_id);
         params["mch_id"] = serde_json::Value::String(conf.mch_id);
         //api 
         let secret_key = conf.secret_key;
-   
+      
         let mut vs = vec![];
         if let Some(map) = params.as_object() {
             for key in map.keys().sorted() {
@@ -44,15 +46,43 @@ impl Order {
             let sign = format!("{:x}",md5::compute(&wait_md5_str)).to_uppercase();
             params["sign"] = serde_json::Value::String(sign);
         }
-
+        
+        
         let body = format!("<xml>{}</xml>",serde_xml_rs::to_string(&params).unwrap_or_default());
+        
         let request = Client::new();
         let r = request.post(&api_url,&body).await.unwrap_or_default();
-
+        
         let ps = serde_xml_rs::from_str::<serde_json::Value>(&r).unwrap_or_default();
-      
+         // 交易类型
+         let trade_type = params["trade_type"].as_str();
+         
         if let Some(root_doms) = ps.as_object() {
             let rr: serde_json::Map<String,serde_json::Value> = root_doms.iter().map(|(i,vo)| (i.clone(),vo["$value"].clone())).collect();
+            
+            let prepayid = rr.get("prepay_id");
+           
+            if trade_type == Some("APP") && prepayid.is_some() { //二次签名
+                let mut new_params = serde_json::Map::new();
+                let mut vs = vec![];
+                let timestamp = Utc::now().naive_local().timestamp();
+                new_params.insert(format!("appid"), rr.get("appid").unwrap().clone());
+                new_params.insert(format!("partnerid"), rr.get("mch_id").unwrap().clone());
+                new_params.insert(format!("prepayid"), rr.get("prepay_id").unwrap().clone());
+                new_params.insert(format!("package"), json!("Sign=WXPay"));
+                new_params.insert(format!("noncestr"), rr.get("nonce_str").unwrap().clone());
+                new_params.insert(format!("timestamp"), json!(timestamp));
+                
+                for key in new_params.keys().sorted() {
+                    vs.push(format!("{}={}", key,new_params[key].to_string().trim_matches(&['"','"'] as &[_])));
+                }
+                vs.push(format!("{}={}", "key",secret_key));    
+                let wait_md5_str = format!("{}",vs.join("&"));
+                let sign = format!("{:x}",md5::compute(&wait_md5_str)).to_uppercase();
+                new_params.insert(format!("sign"), json!(sign));
+                return Ok(serde_json::Value::Object(new_params));
+            }
+
             return Ok(serde_json::Value::Object(rr));
         }
 
